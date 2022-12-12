e6ebd1e86e478de351b5d806f6f24171e282e946
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2022-12-12 13:15:04 +0100

    correct spelling

--- Dropshipping/Custumers.cs
+++ Dropshipping/Custumers.cs
@@ -1,11 +1,11 @@
 namespace DropShipping
 {
 
-public class Custumers
+public class Customers
 {
 public int Id {get; set;}
 public string UserName{get; set;}
-public int password {get; set;}
+public int Password {get; set;}
 public string FirstName {get; set;}
 public string LastName {get; set;}
 public string Address {get; set;}
