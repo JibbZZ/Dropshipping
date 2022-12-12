968185a72a6726da0bf39361f1a02eef7a568a13
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2022-12-12 13:15:42 +0100

    UpdateCustumer

--- Dropshipping/DatabasManager.cs
+++ Dropshipping/DatabasManager.cs
@@ -22,7 +22,7 @@ namespace DropShipping
                 Console.WriteLine(e);
             }
         }
-
+//här är en metod som lägger till produkter i databasen
         public void InsertProducts(Products products)
         {
 
@@ -31,18 +31,31 @@ namespace DropShipping
                       (name, price, description, isHot, onSale, image, weight, sellingPrice, manufacturer)
                       VALUES
                       (@Name, @Price, @Description, @IsHot, @OnSale, @Image, @Weight, @SellingPrice, @Manufacturer)";
-            
-            
+
+
             connection.Execute(sql, products);
 
-            
+
 
         }
+        //här är en metod som hämtar alla produkter från databasen
         public List<Products> GetProducts()
         {
             return connection.Query<Products>("SELECT * FROM Products").ToList();
         }
-    }
+        //här är en metod som uppdaterar Customers
+        public void UpdateCustumers(Customers custumers)
+        {
 
+            string sql = @"
+                      UPDATE `custumers`
+                      SET userName = @UserName, password = @Password
+                      WHERE id = '1'";
+            connection.Execute(sql, custumers);
+
+
+        }
+
+    }
 }
 
