755d7f27999c4da64c5489da09e1e4153369adad
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2022-12-12 11:06:07 +0100

    It cant be null

--- Dropshipping/Products.cs
+++ Dropshipping/Products.cs
@@ -3,15 +3,15 @@ namespace DropShipping
 public class Products
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int Price { get; set; }
-    public string Description { get; set; }
-    public string IsHot { get; set; }
-    public string OnSale { get; set; }
-    public string Image { get; set; }
+    public string Description { get; set; } = "";
+    public string IsHot { get; set; } = "";
+    public string OnSale { get; set; } = "";
+    public string Image { get; set; }  = "";
     public int Weight { get; set; }
     public int SellingPrice { get; set; }
-    public string Manufacturer { get; set; }
+    public string Manufacturer { get; set; } = "";
 
  public Products(){}
 }
