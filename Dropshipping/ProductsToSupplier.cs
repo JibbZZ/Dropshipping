203b3aa51bd680578548a9aa9488551341035b75
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2022-12-13 17:23:32 +0100

    ProductsToSupplier

---
+++ Dropshipping/ProductsToSupplier.cs
@@ -0,0 +1,10 @@
+namespace DropShipping
+{
+    public class Products_To_Supplier
+    {
+            public int ProductId { get; set; }
+            public int SupplierId { get; set; }
+            public int Amount { get; set; }
+    }
+
+}
