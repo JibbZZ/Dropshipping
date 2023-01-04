3038cc473116ae829034b835537f9132524e6266
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2023-01-04 13:06:24 +0100

    Ändrat ordning

--- Dropshipping/Program.cs
+++ Dropshipping/Program.cs
@@ -5,33 +5,29 @@
         static void Main(string[] args)
         {
             DatabasManager db = new DatabasManager();
-            Products products = new Products();
+            Product products = new Product();
             Customers custumers = new Customers();
             DatabaseSeeder databaseSeeder = new DatabaseSeeder(db.connection);
             Order order = new Order();
             Supplier supplier = new Supplier();
             Payment payment = new Payment();
-            new StartMenu().RunMainMenu();
             Ui ui = new Ui();
+            new StartMenu(ui).RunMainMenu();
             Products_To_Supplier products_To_Supplier = new Products_To_Supplier();
             Products_To_Order products_To_Order = new Products_To_Order();
             
-            
-           
-                    
-             
             while (true)
             {
             /* databaseSeeder.CreateProducts_To_OrderTable(products_To_Order);
                databaseSeeder.CreateProducts_To_SupplierTable(products_To_Supplier); */
                //order.AddProduct(product);
               /*  db.GetProductsAndOrder(products, order); */
-              Products product = ui.ChooseProduct();
-               new StartMenu().RunMainMenu();
-               ui.Showproduct(product);
               // ui.CreateProduct(product);
+               Product product = ui.ChooseProduct();
+               ui.Showproduct(product);
                db.UpdateCustumers(custumers);
-
+               new StartMenu(ui).RunMainMenu();
+               
             
             }
         }
