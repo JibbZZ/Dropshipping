a2231257c1edfedc2e8081e5c0b0679a3b5befd7
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2023-01-04 13:07:16 +0100

    Fixes

--- Dropshipping/Ui.cs
+++ Dropshipping/Ui.cs
@@ -7,13 +7,13 @@ namespace DropShipping
 
     public class Ui
     {
-          List<Products> ShopingCartProducts = new List<Products>();
+        
         //choose product
-        public Products ChooseProduct()
+        public Product ChooseProduct()
         {
             DatabasManager db = new DatabasManager();
             Console.Clear();
-            List<Products> products = db.GetProducts();
+            List<Product> products = db.GetProducts();
             string[] options = products.Select(p => p.Name).ToArray();
             int selectedIndex = new Menu("Choose a product!", options).Run();
             Console.Clear();
@@ -22,22 +22,48 @@ namespace DropShipping
          
         
         //buy product
-       /*  public void CreateProduct(Products product)
+          public void CreateProduct(Product product)
         {
             DatabasManager db = new DatabasManager();
-            db.InsertProducts(product);
-            List<Products> products = db.GetProducts();
+            List<Product> products = db.GetProducts();
+            Console.WriteLine(product.Name);
             Console.Clear();
             Console.WriteLine("You bought a product!");
             Console.ReadKey();
-        } */
+        }  
+        //create product
+      
+        //create order
+        public void CreateOrder()
+        {
+            DatabasManager db = new DatabasManager();
+            Order order = new Order();
+            foreach (var item in ShoppingCart.Items)
+            {
+                order.Products.Add(item);
+            }
+            db.InsertOrder(order);
+            Console.Clear();
+            Console.WriteLine("You bought a product!");
+            Console.ReadKey();
+            }
        
 
-        internal void Showproduct(Products product)
+         internal void Showproduct(Product product)
         {
+            DatabasManager db = new DatabasManager();
             Console.Clear();
-            Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Description: {product.Description}, IsHot: {product.IsHot}, OnSale: {product.OnSale}, Image: {product.Image}, Weight: {product.Weight}, SellingPrice: {product.SellingPrice}, Manufacturer: {product.Manufacturer}");
+            Console.WriteLine(@$"Name: {product.Name}
+            Price: {product.Price}
+            Description: {product.Description}
+            IsHot: {product.IsHot}
+            OnSale: {product.OnSale}
+            Image: {product.Image}
+            Weight: {product.Weight}
+            SellingPrice: {product.SellingPrice}
+            Manufacturer: {product.Manufacturer}");
             Console.WriteLine("Do you want to buy this product? (y/n)");
+            
             string answer = Console.ReadLine();
             if (answer == "y")
             {
@@ -49,15 +75,33 @@ namespace DropShipping
                 Console.WriteLine("You didn't buy the product!");
                 Console.ReadKey();
             }
-            Console.ReadKey();
-        }
-        public void AddProductToShopingCart(Products product)
+        
+        } 
+         public void AddProductToShopingCart(Product product)
         {
-            ShopingCartProducts.Add(product);
+            ShoppingCart.Items.Add(product);
             Console.Clear();
+            Console.WriteLine(product.Name + " added to shopping cart");
+            Console.ReadKey();
+            DatabasManager db = new DatabasManager();
             Console.WriteLine("You bought a product!");
             Console.ReadKey();
+            Console.WriteLine("Press any key to go back to the main menu");
+            Console.ReadKey();
+            
+        } 
+
+        public void ShowShoppingCart()
+        {
+            Console.Clear();
+
+          foreach (var item in ShoppingCart.Items)
+          {
+            Console.WriteLine(item.Name + " " + item.Price + "kr");
+            
+          }
+          Console.ReadKey();
         }
-        
+
     }
 }
