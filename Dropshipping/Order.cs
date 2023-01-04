baa49cdba327fd4ff0f93bbda30d15f62b3cfc9a
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2023-01-04 13:06:00 +0100

    La till Lista i Order och en kkonstruktor för att sätta DateTime

--- Dropshipping/Order.cs
+++ Dropshipping/Order.cs
@@ -3,17 +3,20 @@ namespace DropShipping
     public class Order
     {
         public int Id { get; set; }
-        public int CustumerId { get; set; }
+        public int CustomerId { get; set; }
         public int PaymentId { get; set; }
-        public int ProductId { get; set; }
-        public int OrderDate { get; set; }
+        public DateTime OrderDate { get; set; }
         public int OrderStatus { get; set; }
         public int OrderPrice { get; set; }
-        public int ShippingDate { get; set; }
+        public DateTime ShippingDate { get; set; }
         public int ShippingStatus { get; set; }
 
-   
+        public List<Product> Products { get; set; } = new List<Product>();
+        public Order()
+        {
+            OrderDate = DateTime.Now;
+
+        }
     }
-    
 
 }
